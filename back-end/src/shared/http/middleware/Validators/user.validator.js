b9a90816636d1120a68775b9e9b4b2b420c0ae9a
Hiago Briano <hiago.artist@hotmail.com>
2023-02-02 12:15:02 -0300

    Quantidade de caracteres minimo para o nome trocado para 3

--- back-end/src/shared/http/middleware/Validators/user.validator.js
+++ back-end/src/shared/http/middleware/Validators/user.validator.js
@@ -9,7 +9,7 @@ const validateUserLogin = celebrate({
 
 const validateUserRegister = celebrate({
   [Segments.BODY]: Joi.object({
-    name: Joi.string().min(12).required(),
+    name: Joi.string().min(3).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
     role: Joi.string()
