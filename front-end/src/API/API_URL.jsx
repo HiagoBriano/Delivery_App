551b4d5bd49ebabd72f1b3a457898ed78b981ca5
Hiago Briano <hiago.artist@hotmail.com>
2023-03-13 00:02:00 -0300

    branch para deploy

--- front-end/src/API/API_URL.jsx
+++ front-end/src/API/API_URL.jsx
@@ -1,6 +1,6 @@
 const host = process.env.REACT_APP_HOSTNAME || 'localhost';
 const port = process.env.REACT_APP_BACKEND_PORT || '3001';
 
-const API_URL = process.env.REACT_APP_DEPLOY || `http://${host}:${port}/`;
+const API_URL = process.env.REACT_APP_DEPLOY || 'https://delivery-app-backend-jhyg.onrender.com/' || `http://${host}:${port}/`;
 
 export default API_URL;
